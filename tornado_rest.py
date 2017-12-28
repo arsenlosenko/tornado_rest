b0238f0b1600bd9f9ad714b2893884e7c8820638
Arsen Losenko <arsenlosenko@gmail.com>
2017-12-28 17:33:07 +0200

    Created initial project structure, added prices entry point

--- tornado_rest.py
+++ tornado_rest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import logging
+import os
 from tornado.web import RequestHandler, Application
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
@@ -12,17 +12,23 @@ PORT = 8899
 @psql_connection
 def update_books(cur, book, author):
     cur.execute("insert into books (book, author) values ('{}', '{}')".format(book, author))
+    cur.execute("insert into prices (book) values ('{}')".format(book))
 
 
 @psql_connection
-def get_books(cur):
-    cur.execute("select * from books;")
+def update_prices(cur, book, price):
+    cur.execute("insert into prices (book, price) values ('{}', '{}')".format(book, price))
+
+
+@psql_connection
+def get_db_info(cur, table):
+    cur.execute("select * from {};".format(table))
     return cur.fetchall()
 
 
 class MainHandler(RequestHandler):
     def get(self):
-        self.render("api.html")
+        self.render("index.html")
 
 
 class BooksHandler(RequestHandler):
@@ -33,11 +39,15 @@ class BooksHandler(RequestHandler):
     def post(self):
         book = self.get_argument("book")
         author = self.get_argument("author")
-        update_books(book, author)
-        self.write("info updated")
+        if (book is not None and type(book) == str) \
+        and (author is not None and type(author) == str):
+            update_books(book, author)
+            self.write("info updated")
+        else:
+            self.write("check the query, arguments 'book' and 'author' should be used as string")
 
     def format_response(self):
-        books = get_books()
+        books = get_db_info("books")
         response = {"info": [],
                     "ref": "/api/v1/books"}
         for book in books:
@@ -45,11 +55,41 @@ class BooksHandler(RequestHandler):
         return response
 
 
+class PricesHandler(RequestHandler):
+    def get(self):
+        response = self.format_response()
+        self.write(response)
+
+    def post(self):
+        book = self.get_argument("book")
+        price = self.get_argument("price")
+        if (book is not None and type(book) == str) \
+        and (price is not None and type(price) == str):
+            update_prices(book, price)
+            self.write("info updated")
+        else:
+            self.write("check the query, arguments 'book' and 'author' should be used as string")
+
+    def format_response(self):
+        prices = get_db_info("prices")
+        response = {"prices": [],
+                    "ref": "/api/v1/prices"}
+        for price in prices:
+            response['prices'].append({"id": price[0], "book": price[1], "price": price[2]})
+        return response
+
+
 def main():
+    dirname = os.path.dirname(__file__)
+    settings = {
+        "template_path": os.path.join(dirname, "templates"),
+        "static_path": os.path.join(dirname, "static")
+    }
     app = Application([
             (r"/", MainHandler),
-            (r"/api/v1/books/", BooksHandler)
-        ])
+            (r"/api/v1/books/", BooksHandler),
+            (r"/api/v1/prices/", PricesHandler)
+        ], **settings)
     http_server = HTTPServer(app)
     http_server.listen(PORT)
     IOLoop.current().start()
